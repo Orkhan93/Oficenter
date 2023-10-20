3fef2edcf6708457f0627d7d302b6abd6c9ce345
AlayliVusal2000 <alayli2000@mail.ru>
2023-10-20 16:15:08 +0400

    Add project structure and dependencies

---
+++ src/main/java/az/digitalhands/oficenter/OficenterApplication.java
@@ -0,0 +1,13 @@
+package az.digitalhands.oficenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficenterApplication.class, args);
+	}
+
+}
